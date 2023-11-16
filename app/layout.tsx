1f7bd573ec36abdc71dc638845da1b0b750cbf16
luhuaqin <luhuaqin@servyou.com.cn>
2023-11-16 18:03:22 +0800

    ✨ feat: nextjs 告一段落

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/font';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Acme Dashboard',
+    template: '%s | Acme Dashboard'
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router,',
+  metadataBase: new URL('https://next-test-9r36.vercel.app')
+}
 
 export default function RootLayout({
   children,
